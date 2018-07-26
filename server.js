7f8e0004e859274b12e8480261d95c5d4d0b1a0e
gaiyulong <gyl@acewill.cn>
2018-07-26 19:05:43 +0800

    server

--- server.js
+++ server.js
@@ -8,15 +8,23 @@ var options = {
     key:fs.readFileSync('./keys/ca.key'),
     cert:fs.readFileSync('./keys/ca.crt')
 }
+var https_port = 443,
+	http_port = 3000;
 
 var app = express();
 var httpsServer = https.createServer(options,app);
 var httpServer = http.createServer(app);
 
 app.get('/',function(req,res,next){
-        res.send('Hello Express+https');
+	res.send('Welcom to my heart!');
 });
-//https监听3000端口
-httpsServer.listen(443);
-//http监听3001端口
-httpServer.listen(3000);
+//https监听443端口
+httpsServer.listen(https_port, function(){
+	const {address, port} = httpsServer.address();
+	console.log(`https listening at ${address}:${port}`)
+});
+//http监听3000端口
+httpServer.listen(http_port, function() {
+	const {address, port} = httpServer.address();
+	console.log(`http listening at ${address}:${port}`)
+});
