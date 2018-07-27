4681f93bd4efb73309316a2bfdbd2a98ef62c041
gaiyulong <gyl@acewill.cn>
2018-07-27 16:50:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vampire_server
 vampire后台
+
+mac创建一个unix可执行文件? 还能识别js
--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var ejs = require('ejs');
+
+var routers = require('./routes/index');
+
+var app = express();
+app.engine('html', ejs.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routers);
+
+app.use(function(req, res, next) {
+	next(createError(404));
+})
+
+module.exports = app;
