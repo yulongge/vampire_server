54050bf4ba5d6e3b2bd9fcb501987e2c44f6e6d6
yulongge <gaiyulong@gmail.com>
2018-08-01 00:44:08 +0800

    'title'

--- mp_src/public/app.js
+++ mp_src/public/app.js
@@ -1,9 +1,25 @@
 const {initApp} = require('./utils/app_requests');
+const {assign} = require('./utils/object');
+let _app = null;
+
 App({
+	globalData: {},
 	onLaunch() {
-		initApp();
+		_app = this;		
+		initApp(null, {}, rst => {
+			console.log(rst, 'rst');
+			//assign(_app.globalData, rst)
+			if(rst[0].title) {
+				console.log(rst[0].title, 'title')
+				setTimeout(function () {
+					wx.setNavigationBarTitle({
+						title: rst[0].title
+					});
+				}, 200);
+			}
+		});
 	},
 	onShow() {
-	
+		console.log("onShow coming...")
 	}
 })
