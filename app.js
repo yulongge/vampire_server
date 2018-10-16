f8deb1087eccaaba37172e8297495585be7a9ed5
gaiyulong <gyl@acewill.cn>
2018-10-16 16:28:44 +0800

    origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 
 app.all('*', function(req, res, next) {  
-    res.header("Access-Control-Allow-Origin", `*`);
+    res.header("Access-Control-Allow-Origin", `http://localhost:8080`);
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
