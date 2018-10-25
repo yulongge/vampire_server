700dff3343eb4267f230226a124ff9fab8ff1d8b
yulongge <gaiyulong@gmail.com>
2018-10-26 00:48:28 +0800

    'edtor'

---
+++ views/admin/src/pages/Editor/index.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import VampireEditor from 'wangeditor';
+import styles from './style.less';
+
+export default class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            editorContent: ""
+        }
+    }
+
+    componentDidMount() {
+        const { toolbar, content } = this.refs;
+        const editor = new VampireEditor(toolbar, content)
+        // 使用 onchange 函数监听内容的变化，并实时更新到 state 中
+        editor.customConfig.uploadImgShowBase64 = true;
+        editor.customConfig.onchange = html => {
+            this.setState({
+                editorContent: html
+            })
+        }
+        editor.create()
+    }
+
+    render() {
+        return  <div className={styles.editor_page}>
+                    <div className="preview">
+                        <div className="phone" dangerouslySetInnerHTML={{__html: this.state.editorContent}}></div>
+                    </div>
+                    <div className="editorElem">
+                        <div className="toolbar" ref="toolbar"></div>
+                        <div className="content" ref="content"></div>
+                    </div>
+                </div>
+    }
+
+}
