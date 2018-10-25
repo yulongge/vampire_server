13e88562e5d56a5d08b3f4c6da17dae9c863ba3c
gaiyulong <gyl@acewill.cn>
2018-10-25 16:42:07 +0800

    sql

--- sql/article.sql.js
+++ sql/article.sql.js
@@ -1,7 +1,7 @@
 const ARTICLE_LIST = "select * from mp_article";
 
 const ADDARTICLE = (a_title, a_desc, a_url,a_path, a_icon) => {
-	let addSql = `insert into mp_article(a_title, a_desc, a_url, a_path, a_icon) values (${a_title},${a_desc},${a_url},${a_path},${a_icon})`;
+	let addSql = `insert into mp_article(a_title, a_desc, a_url, a_path, a_icon) values (${a_title},${a_desc},${"'" + a_url + "'"},${"'" + a_path + "'"},${"'" + a_icon + "'"})`;
 	console.log(addSql, 'addSql');
 	return addSql;
 };
